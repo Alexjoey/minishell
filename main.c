2839ff8c628f3e77b049e9576b403219bf27f453
Alex Mylle <alexm@live.be>
2024-08-24 16:17:04 +0200

    main to root

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amylle <alexm@live.be>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 16:17:10 by amylle            #+#    #+#             */
+/*   Updated: 2024/07/24 16:20:53 by amylle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minitalk.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	char	*line;
+
+	while (1)
+	{
+	line = readline("minishell ~> ");
+	add_history(line);
+	free (line);
+	}
+}
