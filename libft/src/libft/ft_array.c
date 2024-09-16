1d9d19411e0ac6f3697aa6e3f165a001bd5a7026
Alex Mylle <alexm@live.be>
2024-09-16 05:15:58 +0200

    added proper libft

---
+++ libft/src/libft/ft_array.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_array.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amylle <alexm@live.be>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 15:57:39 by amylle            #+#    #+#             */
+/*   Updated: 2024/07/24 16:03:03 by amylle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/libft.h"
+
+void	ft_freearray(char **array)
+{
+	int	i;
+
+	i = -1;
+	while (array[++i])
+		free (array[i]);
+	free (array);
+}
+
+char	**ft_duparray(char **array)
+{
+	int			i;
+	char	**ret;
+
+	i = -1;
+	while (array[++i]);
+	ret = ft_calloc(i, sizeof(char *));
+	i = -1;
+	while (array[++i])
+		ret[i] = ft_strdup(array[i]);
+	return (ret);
+}
